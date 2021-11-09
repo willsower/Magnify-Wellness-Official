56eb75f74d387629657a7f1fdab5f213026fd202
Taichen <jatr812@gmail.com>
2021-11-08 17:46:10 -0800

    Adding in events page

--- pages/contact.js
+++ pages/contact.js
@@ -2,8 +2,8 @@ import Layout from "../components/layout";
 
 export default function Contact() {
   return (
-    <Layout title = "Contact">
-      
+    <Layout title="Contact">
+      <h1 className="text-center text-5xl">Contact</h1>
     </Layout>
   );
 }
