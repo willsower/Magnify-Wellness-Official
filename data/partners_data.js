3a3c7d6a3531aed31b65e3705c7268d014463cf3
Taichen <jatr812@gmail.com>
2021-11-08 17:14:33 -0800

    Added partner data
    Adding in partner data

---
+++ data/partners_data.js
@@ -0,0 +1,54 @@
+export const partners = [
+  {
+    name: "Creator Tribe",
+    image: "creator_tribe.png",
+  },
+  {
+    name: "Girl Genius Magazine",
+    image: "girl_genius_magazine.png",
+  },
+  {
+    name: "Go Fund Teen",
+    image: "go_fund_teen.jpg",
+  },
+  {
+    name: "Hack Club Bank",
+    image: "hack_club_bank.png",
+  },
+  {
+    name: "Hack Club",
+    image: "hack_club.png",
+  },
+  {
+    name: "HEARO UW",
+    image: "hearo_uw.png",
+  },
+  {
+    name: "Inspiration Fine Arts",
+    image: "inspiration_fine_arts.png",
+  },
+  {
+    name: "Just Ask A Teen",
+    image: "just_ask_a_teen.png",
+  },
+  {
+    name: "Proximity",
+    image: "proximity.png",
+  },
+  {
+    name: "Ryerson University Hacks",
+    image: "ryerson_university_hacks.png",
+  },
+  {
+    name: "Shattering The Stigma",
+    image: "shattering_the_stigma.jpg",
+  },
+  {
+    name: "Sticker Giant",
+    image: "sticker_giant.png",
+  },
+  {
+    name: "The Paris Project",
+    image: "the_paris_project.png",
+  },
+];
