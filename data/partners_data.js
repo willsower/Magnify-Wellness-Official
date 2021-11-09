641be119de3f7229c06c0634b90f7f60ade52e95
Taichen <jatr812@gmail.com>
2021-11-08 17:29:06 -0800

    Adding in links and one more image

--- data/partners_data.js
+++ data/partners_data.js
@@ -2,53 +2,71 @@ export const partners = [
   {
     name: "Creator Tribe",
     image: "creator_tribe.png",
+    link: "/partnerships"
   },
   {
     name: "Girl Genius Magazine",
     image: "girl_genius_magazine.png",
+    link: "https://girlgeniusmagazine.wixsite.com/official",
   },
   {
     name: "Go Fund Teen",
     image: "go_fund_teen.jpg",
+    link: "https://gofundteen.com/",
   },
   {
     name: "Hack Club Bank",
     image: "hack_club_bank.png",
+    link: "https://hackclub.com/bank/",
   },
   {
     name: "Hack Club",
     image: "hack_club.png",
+    link: "https://hackclub.com/",
   },
   {
     name: "HEARO UW",
     image: "hearo_uw.png",
+    link: "https://beacons.ai/hearo.uw",
   },
   {
     name: "Inspiration Fine Arts",
     image: "inspiration_fine_arts.png",
+    link: "https://www.inspirationfinearts.org/",
   },
   {
     name: "Just Ask A Teen",
     image: "just_ask_a_teen.png",
+    link: "https://sites.google.com/view/just-ask-a-teen/home",
   },
   {
     name: "Proximity",
     image: "proximity.png",
+    link: "https://www.proximityfoundation.org/",
   },
   {
     name: "Ryerson University Hacks",
     image: "ryerson_university_hacks.png",
+    link: "https://www.ruhacks.com/",
   },
   {
     name: "Shattering The Stigma",
     image: "shattering_the_stigma.jpg",
+    link: "https://shatteringthestigma.wixsite.com/project",
   },
   {
     name: "Sticker Giant",
     image: "sticker_giant.png",
+    link: "https://www.stickergiant.com/",
   },
   {
     name: "The Paris Project",
     image: "the_paris_project.png",
+    link: "https://www.instagram.com/the__paris__project/?hl=en",
+  },
+  {
+    name: "Project Calendula",
+    image: "project_calendula.webp",
+    link: "https://www.projectcalendula.com/",
   },
 ];
