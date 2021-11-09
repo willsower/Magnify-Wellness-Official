044cefb3f74682a592415fe436bdad0dfbb11e13
Taichen <jatr812@gmail.com>
2021-11-08 17:36:53 -0800

    Updated size of partnerships

--- pages/partnerships.js
+++ pages/partnerships.js
@@ -7,12 +7,12 @@ import { partners } from "../data/partners_data";
 
 // Fetch data on client side
 export async function getStaticProps() {
-    return {
-      props: {
-        data: partners,
-      },
-    };
-  }
+  return {
+    props: {
+      data: partners,
+    },
+  };
+}
 
 export default function Partnerships({ data }) {
   return (
@@ -21,14 +21,18 @@ export default function Partnerships({ data }) {
       <h5 className="text-2xl text-center mt-12">Partner with us!</h5>
 
       {/* Partners */}
-      <div className = "mt-12 grid max-w-xs m-auto sm:grid-cols-2 sm:max-w-md md:grid-cols-3 md:max-w-2xl hover:cursor-pointer">
-          {data.map((partner => 
-            <div className = "mt-4 m-auto">
-                <Link href = "/">
-                    <Image src = {`/img/partners/${partner.image}`} height = "200" width = "200"/>
-                </Link>
-            </div>
-          ))}
+      <div className="mt-12 grid max-w-xs m-auto sm:grid-cols-2 sm:max-w-md md:grid-cols-3 md:max-w-2xl hover:cursor-pointer">
+        {data.map((partner) => (
+          <div className="mt-4 m-auto relative w-44 h-44">
+            <Link href={partner.link}>
+              <Image
+                src={`/img/partners/${partner.image}`}
+                layout="fill"
+                objectFit="contain"
+              />
+            </Link>
+          </div>
+        ))}
       </div>
     </Layout>
   );
