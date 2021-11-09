b25c1d06359b4194be264ed744fd2c31dae487ce
Taichen <jatr812@gmail.com>
2021-11-08 17:57:02 -0800

    Adding app title

--- pages/app.js
+++ pages/app.js
@@ -2,8 +2,8 @@ import Layout from "../components/layout";
 
 export default function App() {
   return (
-    <Layout title = "App">
-      
+    <Layout title="App">
+      <h1 className="text-center text-5xl">The Magnify Wellness App</h1>
     </Layout>
   );
 }
